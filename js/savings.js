b8e9c238d8465fde7926fd9a72b560ed0a5ca673
sdshuvo98 <hellosdshuvo@gmail.com>
2022-02-17 22:44:28 +0600

    halde error

--- js/savings.js
+++ js/savings.js
@@ -5,6 +5,7 @@ function getInputValue (input) {
     const valueNumber = parseInt(valueAmont);
     return valueNumber;
 }
+// get text 
 function getInputText (textInput){
     const text = document.getElementById(textInput);
     return text; 
@@ -13,7 +14,6 @@ function getInputText (textInput){
 // calculate balance
 document.getElementById("calculate-btn").addEventListener("click", function() {
     // get value
-    // debugger;
     const income = getInputValue("income");
     const foodCost = getInputValue("food-cost");
     const rentCost = getInputValue("rent-cost");
@@ -21,6 +21,7 @@ document.getElementById("calculate-btn").addEventListener("click", function() {
     // get expence 
     if (income>0 && foodCost>0 && rentCost>0 && clothesCost>0){
         const totalExpence = foodCost+rentCost+clothesCost;
+        // error handle 
         if(totalExpence>income){
             alert("Your expence is bigger then your income!")
             return income;
@@ -39,10 +40,19 @@ document.getElementById("calculate-btn").addEventListener("click", function() {
 document.getElementById("save-btn").addEventListener("click", function(){
     const income = getInputValue("income");
     const save = getInputValue("save");
-    const saving = income*(save/100);
-    const savingAmount = getInputText("saving-amount");
-    savingAmount.innerText = saving;
-    const balance = getInputText("balance");
-    const remainingAmount = getInputText("remaining-amount");
-    remainingAmount.innerText = balance.innerText - saving;
+    if(save>0){
+        const saving = income*(save/100);
+        const savingAmount = getInputText("saving-amount");
+        savingAmount.innerText = saving;
+        const balance = getInputText("balance");
+        const remainingAmount = getInputText("remaining-amount");
+        if(balance.innerText<saving){
+            alert("Your saving ammount is bigger then your rest amount!")
+            return save;
+        }
+        remainingAmount.innerText = balance.innerText - saving;
+    }
+    else{
+        alert("Please insert a positive valid number!");
+    }
 })
